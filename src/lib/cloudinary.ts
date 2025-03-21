6312009ace160ed7265e321d2663f2d54dc8dba4
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-03-21 09:40:32 -0500

    fix cloudinary config

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,5 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config(process.env.CLOUDINARY_URL ?? "");
+const cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
+const api_key = process.env.CLOUDINARY_API_KEY;
+const api_secret = process.env.CLOUDINARY_API_SECRET;
 
-export default cloudinary
+cloudinary.config({
+  cloud_name,
+  api_key,
+  api_secret,
+});
+
+export default cloudinary;

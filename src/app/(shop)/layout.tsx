47fbb987931b2d479b8ebe62edf2005d0102e819
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-03-21 10:15:20 -0500

    change import from bareel file

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,5 +1,6 @@
-import { Sidebar, TopMenu } from "@/components";
-import { Footer } from "@/components";
+import { Footer } from "@/components/ui/footer/Footer";
+import { Sidebar } from "@/components/ui/sidebar/Sidebar";
+import { TopMenu } from "@/components/ui/top-menu/TopMenu";
 
 export default function ShopLayout({
   children,
